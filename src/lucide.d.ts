34afdb11f1e414362769b2ea22925f67f0376375
Africaorg-star <maxcameo09@gmail.com>
2025-07-28 21:15:31 +0300

    Create lucide.d.ts

---
+++ src/lucide.d.ts
@@ -0,0 +1,5 @@
+declare module 'lucide-react/dist/esm/icons/*.js' {
+  import { LucideProps } from 'lucide-react';
+  const component: (props: LucideProps) => JSX.Element;
+  export default component;
+}
